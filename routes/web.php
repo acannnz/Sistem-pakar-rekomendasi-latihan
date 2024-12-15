710690f48d84ffb62fee4cee25704d8fee753bd1
Candra Pratama <putu.candra689@gmail.com>
2024-12-15 23:30:01 +0800

    update route

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ExerciseRecommendationController;
 
-Route::get('/', function () {
-    return view('template');
-});
 Route::get('/index', function () {
     return view('index');
 });
