51a287d6d4841a0fe0d06204848e90b2daddad59
Candra Pratama <putu.candra689@gmail.com>
2024-12-17 17:09:02 +0800

    nambah table baru

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ExerciseRecommendationController;
 
+Route::get('/', function () {
+    return view('template');
+});
 Route::get('/index', function () {
     return view('index');
 });
